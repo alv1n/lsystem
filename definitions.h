eadd4a24827243cf832be0d3bd087b5a18893a2e
Alvin <alvinreumer@gmail.com>
2017-08-13 12:03:59 +0200

    Modified values

--- definitions.h
+++ definitions.h
@@ -9,17 +9,17 @@
 #include <allegro5/allegro_primitives.h>
 #include "definitions.h"
 
-#define min(a,b) (a<b?a:b)
-#define max(a,b) (a>b?a:b)
+//#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
+//#define max(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 #define PI 3.14159265 
-#define SCREEN_W 640
-#define SCREEN_H 480
+#define SCREEN_W 1000
+#define SCREEN_H 1000
 
-#define STRING_SIZE 1000
-#define RULES_SIZE 1000
-#define RULES_AMOUNT 40
-#define MAX_STRING 1000
+#define STRING_SIZE 40000
+#define RULES_SIZE 40000
+#define RULES_AMOUNT 400
+#define MAX_STRING 40000
 
 
 typedef struct
